ae5700b363b90fb295b2bfef14f9f7799e2bfb3f
Andy Doro <mail@andydoro.com>
2020-05-12 21:23:08 -0400

    added print dir
    print dir helper function

--- CircuitPython/code.py
+++ CircuitPython/code.py
@@ -13,7 +13,7 @@
 # - add stereo playback
 #
 
-
+import os
 import time
 import board
 import audiomp3
@@ -46,6 +46,40 @@ vfs = storage.VfsFat(sdcard)
 storage.mount(vfs, "/sd")
 
 
+# Use the filesystem as normal! Our files are under /sd
+# This helper function will print the contents of the SD
+
+def print_directory(path, tabs=0):
+    for file in os.listdir(path):
+        stats = os.stat(path + "/" + file)
+        filesize = stats[6]
+        isdir = stats[0] & 0x4000
+
+        if filesize < 1000:
+            sizestr = str(filesize) + " by"
+        elif filesize < 1000000:
+            sizestr = "%0.1f KB" % (filesize / 1000)
+        else:
+            sizestr = "%0.1f MB" % (filesize / 1000000)
+
+        prettyprintname = ""
+        for _ in range(tabs):
+            prettyprintname += "   "
+        prettyprintname += file
+        if isdir:
+            prettyprintname += "/"
+        print('{0:<40} Size: {1:>10}'.format(prettyprintname, sizestr))
+
+        # recursively print directory contents
+        if isdir:
+            print_directory(path + "/" + file, tabs + 1)
+
+
+print("Files on filesystem:")
+print("====================")
+print_directory("/sd")
+
+
 i2c = io.I2C(board.SCL, board.SDA)  # Change to the appropriate I2C clock & data
 # pins here!
 
@@ -65,7 +99,6 @@ a = audioio.AudioOut(board.A0, right_channel=board.A1) # stereo sound through A0
 # 24 hour time
 playhour = 19 
 playmin = 0
-
 
 # pylint: disable-msg=bad-whitespace
 # pylint: disable-msg=using-constant-test
